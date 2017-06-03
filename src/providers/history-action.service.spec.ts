620e83c77a491b8acb0b56365d86bfb347c15de0
Salvador Subarroca <subarroca@gmail.com>
2017-06-03 11:55:51 +0200

    feat(history-action): create service

--- src/providers/history-action.service.spec.ts
+++ src/providers/history-action.service.spec.ts
@@ -6,6 +6,25 @@ import { HistoryActionService } from './history-action.service';
 
 
 
+const action1 = new HistoryAction({
+  title: 'action1'
+});
+const action2 = new HistoryAction({
+  title: 'action2'
+});
+const action3 = new HistoryAction({
+  title: 'action3'
+});
+const action4 = new HistoryAction({
+  title: 'action4'
+});
+const action5 = new HistoryAction({
+  title: 'action5'
+});
+const action6 = new HistoryAction({
+  title: 'action6'
+});
+
 describe('Provider: HistoryAction', () => {
   let service;
 
@@ -23,13 +42,35 @@ describe('Provider: HistoryAction', () => {
 
   it('has no actions on start',
     inject([HistoryActionService], (service) => {
+      expect(service.actions.length).toEqual(0);
     }));
 
   it('adds an action',
     inject([HistoryActionService], (service) => {
+      service.addAction(action1);
+
+      expect(service.actions[0]).toEqual(action1);
     }));
 
   it('returns a short ordered list',
     inject([HistoryActionService], (service) => {
+      // TODO: find a better way to work with dates in test
+      action1.createdAt = new Date(1);
+      action2.createdAt = new Date(2);
+      action3.createdAt = new Date(3);
+      action4.createdAt = new Date(4);
+      action5.createdAt = new Date(5);
+      action6.createdAt = new Date(6);
+
+      service.addAction(action1);
+      service.addAction(action2);
+      service.addAction(action3);
+      service.addAction(action4);
+      service.addAction(action5);
+      service.addAction(action6);
+
+      expect(service.orderedActions.length).toEqual(5);
+      expect(service.orderedActions[0]).toBe(action6);
+      expect(service.orderedActions[4]).toBe(action2);
     }));
 });
--- src/providers/history-action.service.ts
+++ src/providers/history-action.service.ts
@@ -1,8 +1,27 @@
+import { Storage } from '@ionic/storage';
 import { Injectable } from '@angular/core';
+
+
+import { HistoryAction } from './../models/history-action';
+import { TodoSection } from './../models/todo-section';
 
 
 @Injectable()
 export class HistoryActionService {
+  actions: HistoryAction[] = [];
+
+  private displayableAmount = 5;
+
   constructor() { }
+
+  addAction(action: HistoryAction) {
+    this.actions.push(action);
+  }
+
+  get orderedActions() {
+    return this.actions
+      .sort((a, b) => +a.createdAt < +b.createdAt ? 1 : -1)
+      .slice(0, this.displayableAmount);
+  }
 
 }
