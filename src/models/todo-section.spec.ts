0db19311bc7648c612106557699f39aa55bc4bab
Salvador Subarroca <subarroca@gmail.com>
2017-06-03 11:15:16 +0200

    feat(todo-section): add test description

---
+++ src/models/todo-section.spec.ts
@@ -0,0 +1,25 @@
+describe('Model: TodoSection', () => {
+  it('initialises with a title', () => {
+  });
+
+  it('is empty by default', () => {
+  });
+
+  it('contains defined items', () => {
+  });
+
+  it('flushes', () => {
+  });
+
+  it('adds items', () => {
+  });
+
+  it('removes items', () => {
+  });
+
+  it('removes nothing if item is not present', () => {
+  });
+
+  it('returns ordered items', () => {
+  });
+})
