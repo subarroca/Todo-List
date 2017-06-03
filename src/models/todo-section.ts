5eb6430f6babe4285ae76267727bae5d8a9d4245
Salvador Subarroca <subarroca@gmail.com>
2017-06-03 11:41:07 +0200

    feat(todo-section): add model

---
+++ src/models/todo-section.ts
@@ -0,0 +1,35 @@
+import { TodoItem } from './todo-item';
+
+
+
+export class TodoSection {
+  title: string;
+  items: TodoItem[];
+
+  constructor(options: {
+    title: string,
+    items?: TodoItem[]
+  }) {
+    if (options) {
+      this.title = options.title;
+      this.items = options.items || [];
+    }
+  }
+
+  flush() {
+    this.items = [];
+  }
+
+  addItem(item: TodoItem) {
+    this.items.push(item);
+  }
+
+  removeItem(item: TodoItem) {
+    this.items = this.items
+      .filter(localItem => !Object.is(item, localItem));
+  }
+
+  get orderedItems() {
+    return this.items.sort((a, b) => +a.createdAt > +b.createdAt ? -1 : 1);
+  }
+}
