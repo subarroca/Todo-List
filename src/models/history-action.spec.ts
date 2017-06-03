bcbde825f5efa16cc2608cbd89fff4118bc42a00
Salvador Subarroca <subarroca@gmail.com>
2017-06-03 11:14:24 +0200

    feat(history-action): add test description

---
+++ src/models/history-action.spec.ts
@@ -0,0 +1,13 @@
+describe('Model: HistoryAction', () => {
+  it('initialises with a title', () => {
+  });
+
+  it('has current timestamp', () => {
+  });
+
+  it('contains defined items', () => {
+  });
+
+  it('returns ordered items', () => {
+  });
+})
