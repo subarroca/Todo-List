ab0ee1e7212d769e40ada4a165a8b9f55ea57198
Salvador Subarroca <subarroca@gmail.com>
2017-06-03 11:14:47 +0200

    feat(todo-section): add test description

---
+++ src/models/todo-item.spec.ts
@@ -0,0 +1,19 @@
+describe('Model: TodoItem', () => {
+  it('initialises with a title', () => {
+  });
+
+  it('is done if declared', () => {
+  });
+
+  it('is marked as TODO by default', () => {
+  });
+
+  it('has a current timestamp', () => {
+  });
+
+  it('toggles from TODO to DONE', () => {
+  });
+
+  it('toggles from DONE to TODO', () => {
+  });
+})
